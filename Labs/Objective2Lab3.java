0709a3ba6cedc3ecebd9a237014a8033b13e84dc
JKantor <boomersooner918@gmail.com>
2023-01-02 15:31:13 -0700

    finished Obj2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    int born = 1992;
+    String month = "August";
+    boolean isWeekend = false;
+    System.out.println(born);
+    System.out.println(month);
+    System.out.println(isWeekend);
+  }
+}
