06ae5ef5a096d4f49a29e7797bb06ebd21964920
JKantor <boomersooner918@gmail.com>
2023-01-02 15:07:43 -0700

    finished Obj2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Jay Kantor";
+    int age = 30;
+    String state = "Oklahoma";
+    System.out.println("Name: "+name+" Age: "+age+" State: "+state);
+  }
+}
