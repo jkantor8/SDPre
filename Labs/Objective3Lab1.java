762afd7c790544fa8761807fe1ed5066b65127cc
JKantor <boomersooner918@gmail.com>
2023-01-02 17:23:11 -0700

    finished Obj3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,10 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+      num1 = 8;
+      num2 = 55;
+      num3 = 18;
+      average = (num1+num2+num3) / 3;
+      System.out.println(average);
+  }
+}
