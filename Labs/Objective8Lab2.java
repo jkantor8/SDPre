1304effe708adf3611a3e18bc15d06c6a9a962c7
JKantor <boomersooner918@gmail.com>
2023-01-05 20:05:10 -0700

    finished Obj8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,8 @@
+public class Objective8Lab2 {
+	public static void main(String[] args) {
+		int sum = 0;
+		for(int control = 1; control <= 20; control += 1) {
+			sum = control + sum;}
+			System.out.println(sum);
+		}
+	}
